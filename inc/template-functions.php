c735367c6a4e95577a0225eede91a8c05ce0b579
Blackharddd <djalexmurcer@gmail.com>
2020-12-29 20:49:23 +0200

    Added analytics tags.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -38,6 +38,42 @@ add_action( 'wp_head', 'pdp_pingback_header' );
 
 
 /**
+ * Add analytics
+ */
+function pdp_add_analytics(){ ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-57306807-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-57306807-1');
+    </script>
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '707195393025403');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" src="https://www.facebook.com/tr?id=707195393025403&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
+	<?php
+}
+add_action( 'wp_head', 'pdp_add_analytics' );
+
+
+/**
  * Add appointment button to main menu.
  */
 add_filter( 'wp_nav_menu_items', 'pdp_menu_add_appointments', 10, 2 );
