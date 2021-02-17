1077f77a50cebc95ae298e6f191ab993566c8ad5
Blackharddd <djalexmurcer@gmail.com>
2021-02-17 15:36:21 +0200

    Changed analytics adding function.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -40,35 +40,8 @@ add_action( 'wp_head', 'pdp_pingback_header' );
 /**
  * Add analytics
  */
-function pdp_add_analytics(){ ?>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-57306807-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-57306807-1');
-    </script>
-
-    <!-- Facebook Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window,document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '707195393025403');
-        fbq('track', 'PageView');
-    </script>
-    <noscript>
-        <img height="1" width="1" src="https://www.facebook.com/tr?id=707195393025403&ev=PageView&noscript=1"/>
-    </noscript>
-    <!-- End Facebook Pixel Code -->
-	<?php
+function pdp_add_analytics(){
+	echo carbon_get_theme_option( 'analytics_code' );
 }
 add_action( 'wp_head', 'pdp_add_analytics' );
 
@@ -79,8 +52,8 @@ add_action( 'wp_head', 'pdp_add_analytics' );
 add_filter( 'wp_nav_menu_items', 'pdp_menu_add_appointments', 10, 2 );
 
 function pdp_menu_add_appointments( $items, $args ){
-    if( 'header-menu' == $args->theme_location ){
-        ob_start(); ?>
+	if( 'header-menu' == $args->theme_location ){
+		ob_start(); ?>
         <li class="menu-item menu-item_appointment" ref="cartToggler">
             <button class="oce-cart" @click="isCartActive = !isCartActive">
                 запись
@@ -89,11 +62,11 @@ function pdp_menu_add_appointments( $items, $args ){
                 </div>
             </button>
         </li>
-        <?php
-        $items .= ob_get_clean();
-    }
+		<?php
+		$items .= ob_get_clean();
+	}
 
-    return $items;
+	return $items;
 }
 
 
@@ -103,13 +76,13 @@ function pdp_menu_add_appointments( $items, $args ){
 add_action( 'wp_footer', 'pdp_add_right_appointment_button' );
 
 function pdp_add_right_appointment_button(){
-    global $post;
-    if( $post->ID != 66 ){ ?>
+	global $post;
+	if( $post->ID != 66 ){ ?>
         <div class="sticky-btns">
             <button class="btn-sticky-right sticky-btns__btn" data-micromodal-trigger="modal-appointment">online запись</button>
         </div>
-    <?php
-    }
+		<?php
+	}
 }
 
 
@@ -128,12 +101,12 @@ function pdp_add_appointment_modal(){ ?>
                 <button class="modal__close btn-icon" aria-label="Close modal" data-micromodal-close><svg width="14" height="14" fill="none"><path d="M14 1.4L12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7 14 1.4z" fill="#000"/></svg></button>
 
                 <div class="modal__content">
-                    <?php get_template_part( 'templates/forms/appointment-quick' ); ?>
+					<?php get_template_part( 'templates/forms/appointment-quick' ); ?>
                 </div>
             </div>
         </div>
     </div>
-<?php
+	<?php
 }
 
 
@@ -144,7 +117,7 @@ function pdp_add_appointment_modal(){ ?>
 add_action( 'wp_footer', 'pdp_add_service_category_appointment_modal' );
 
 function pdp_add_service_category_appointment_modal(){
-    if( is_page_template( 'service-category.php' ) ){ ?>
+	if( is_page_template( 'service-category.php' ) ){ ?>
         <div class="modal" id="modal-service-category-appointment" aria-hidden="true">
             <div class="modal__dimmer" data-micromodal-close>
                 <div class="modal__inner" role="dialog" aria-modal="true">
@@ -153,13 +126,13 @@ function pdp_add_service_category_appointment_modal(){
                     <button class="modal__close btn-icon" aria-label="Close modal" data-micromodal-close><svg width="14" height="14" fill="none"><path d="M14 1.4L12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7 14 1.4z" fill="#000"/></svg></button>
 
                     <div class="modal__content">
-                        <?php get_template_part( 'templates/forms/appointment-service-category' ); ?>
+						<?php get_template_part( 'templates/forms/appointment-service-category' ); ?>
                     </div>
                 </div>
             </div>
         </div>
-        <?php
-    }
+		<?php
+	}
 }
 
 
@@ -170,7 +143,7 @@ function pdp_add_service_category_appointment_modal(){
 add_action( 'wp_footer', 'pdp_add_share_post_modal' );
 
 function pdp_add_share_post_modal(){
-    if( is_single() ){ ?>
+	if( is_single() ){ ?>
         <div class="modal" id="modal-share-post" aria-hidden="true">
             <div class="modal__dimmer" data-micromodal-close>
                 <div class="modal__inner">
@@ -192,8 +165,8 @@ function pdp_add_share_post_modal(){
                 </div>
             </div>
         </div>
-	<?php
-    }
+		<?php
+	}
 }
 
 
@@ -202,13 +175,13 @@ function pdp_add_share_post_modal(){
  */
 
 function pdp_add_promotions_modal( $promotions = array() ){
-    $html = '<div class="promotion-details-wrap">';
+	$html = '<div class="promotion-details-wrap">';
 
-    foreach( $promotions as $key => $promotion ){
-        $content = wpautop( $promotion->post_content );
-        $thumbnail = get_the_post_thumbnail( $promotion->ID, 'medium' );
+	foreach( $promotions as $key => $promotion ){
+		$content = wpautop( $promotion->post_content );
+		$thumbnail = get_the_post_thumbnail( $promotion->ID, 'medium' );
 
-        $html .= "
+		$html .= "
             <div class='promotion-details' data-promotion='{$promotion->post_title}'>
                 <div class='promotion-details__header'>{$promotion->post_title}</div>
                 <div class='promotion-details__body'>
@@ -216,9 +189,9 @@ function pdp_add_promotions_modal( $promotions = array() ){
                 </div>
             </div>
         ";
-    }
+	}
 
-    $html .= '</div>';
+	$html .= '</div>';
 
 	add_action( 'wp_footer', function() use ( &$html ) { ?>
         <div class="modal" id="modal-promotions" aria-hidden="true">
@@ -226,14 +199,14 @@ function pdp_add_promotions_modal( $promotions = array() ){
                 <div class="modal__inner">
                     <button class="modal__close btn-icon" aria-label="Close modal" data-micromodal-close><svg width="14" height="14" fill="none"><path d="M14 1.4L12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7 14 1.4z" fill="#000"/></svg></button>
                     <div class="modal__content">
-                        <?=$html; ?>
-                        <?php get_template_part( 'templates/forms/appointment-promotion' ); ?>
+						<?=$html; ?>
+						<?php get_template_part( 'templates/forms/appointment-promotion' ); ?>
                     </div>
                 </div>
             </div>
         </div>
-    <?php
-    } );
+		<?php
+	} );
 }
 
 
@@ -257,20 +230,20 @@ function pdp_pagination_template( $template, $class ){
  */
 
 function pdp_register_image_sizes(){
-    if( function_exists( 'add_image_size' ) ){
-        add_image_size( 'testimonial', 160, 160, true );
-        add_image_size( 'salons-slider-thumb', 270, 212, true );
-        add_image_size( 'services-slider-thumb', 340, 340, true );
-        add_image_size( 'salon-carousel-large', 1260, 800, true );
-        add_image_size( 'salon-carousel-nav', 248, 154, true );
-        add_image_size( 'salon-master', 291, 400, true );
+	if( function_exists( 'add_image_size' ) ){
+		add_image_size( 'testimonial', 160, 160, true );
+		add_image_size( 'salons-slider-thumb', 270, 212, true );
+		add_image_size( 'services-slider-thumb', 340, 340, true );
+		add_image_size( 'salon-carousel-large', 1260, 800, true );
+		add_image_size( 'salon-carousel-nav', 248, 154, true );
+		add_image_size( 'salon-master', 291, 400, true );
 
-	    /**
-	     *  Blog
-	     */
-        add_image_size( 'blog-header', 345, 345, true );
-        add_image_size( 'blog-thumbnail', 300, 300, true );
-    }
+		/**
+		 *  Blog
+		 */
+		add_image_size( 'blog-header', 345, 345, true );
+		add_image_size( 'blog-thumbnail', 300, 300, true );
+	}
 }
 
 pdp_register_image_sizes();
@@ -296,13 +269,13 @@ function pdp_search_posts_filter( $query ){
  */
 
 function pdp_posts_sort_toggler(){
-    $order = 'asc';
+	$order = 'asc';
 
-    if( isset( $_GET['order'] ) && $_GET['order'] == 'asc' ){
-        $order = 'desc';
-    }
+	if( isset( $_GET['order'] ) && $_GET['order'] == 'asc' ){
+		$order = 'desc';
+	}
 
-    echo '<a href="?orderby=date&order=' . $order . '" class="blog-filter__sort-btn btn-icon"><svg width="18" height="12" fill="none"><path d="M0 12h6v-2H0v2zM0 0v2h18V0H0zm0 7h12V5H0v2z" fill="#000"/></svg></a>';
+	echo '<a href="?orderby=date&order=' . $order . '" class="blog-filter__sort-btn btn-icon"><svg width="18" height="12" fill="none"><path d="M0 12h6v-2H0v2zM0 0v2h18V0H0zm0 7h12V5H0v2z" fill="#000"/></svg></a>';
 }
 
 
@@ -311,7 +284,7 @@ function pdp_posts_sort_toggler(){
  */
 
 function pdp_tags_cloud(){
-    $tags = get_tags();
+	$tags = get_tags();
 	$current_tag = get_queried_object();
 	$html = '';
 
@@ -321,7 +294,7 @@ function pdp_tags_cloud(){
 
 		if( $current_tag->slug == $tag->slug ){
 			$tag_classes .= ' active';
-        }
+		}
 
 		$html .= "<a href='{$tag_link}' title='{$tag->name} тег' class='{$tag_classes}'>";
 		$html .= "#{$tag->name}</a>";
@@ -336,12 +309,12 @@ function pdp_tags_cloud(){
  */
 
 function pdp_get_post_likes(){
-    if( get_post_meta( get_the_ID(), '_likes', true ) ){
-        return get_post_meta( get_the_ID(), '_likes', true );
-    }
-    else{
-        return 0;
-    }
+	if( get_post_meta( get_the_ID(), '_likes', true ) ){
+		return get_post_meta( get_the_ID(), '_likes', true );
+	}
+	else{
+		return 0;
+	}
 }
 
 
@@ -350,19 +323,19 @@ function pdp_get_post_likes(){
  */
 
 function pdp_get_salon_masters( $salon_term_id ){
-    return get_posts(
-        array(
-            'posts_per_page'    => -1,
-            'post_type'         => 'master',
-            'tax_query'         => array(
-                array(
-                    'taxonomy'  => 'salon',
-                    'field'     => 'id',
-                    'terms'     => $salon_term_id
-                )
-            )
-        )
-    );
+	return get_posts(
+		array(
+			'posts_per_page'    => -1,
+			'post_type'         => 'master',
+			'tax_query'         => array(
+				array(
+					'taxonomy'  => 'salon',
+					'field'     => 'id',
+					'terms'     => $salon_term_id
+				)
+			)
+		)
+	);
 }
 
 
@@ -371,51 +344,51 @@ function pdp_get_salon_masters( $salon_term_id ){
  */
 
 function pdp_get_related_pages( $post = false ){
-    if( !$post ){
-        return false;
-    }
-    else{
-	    if( $post->post_parent != 0 ){
-		    return get_children( array(
-			    'post_type'     => 'page',
-			    'numberposts'   => 4,
-			    'orderby'       => 'rand',
-			    'order'         => 'ASC',
-			    'post_parent'   => $post->post_parent,
-			    'exclude'       => $post->ID
-		    ) );
-        }
-	    else{
-	        $pages = array(
-	            117,
-                108,
-                98,
-                88,
-                130,
-                132
-            );
+	if( !$post ){
+		return false;
+	}
+	else{
+		if( $post->post_parent != 0 ){
+			return get_children( array(
+				'post_type'     => 'page',
+				'numberposts'   => 4,
+				'orderby'       => 'rand',
+				'order'         => 'ASC',
+				'post_parent'   => $post->post_parent,
+				'exclude'       => $post->ID
+			) );
+		}
+		else{
+			$pages = array(
+				117,
+				108,
+				98,
+				88,
+				130,
+				132
+			);
 
-		    if( ($key = array_search( $post->ID, $pages ) ) !== false ){
-			    unset( $pages[$key] );
-			    $pages = array_values( $pages );
-		    }
+			if( ($key = array_search( $post->ID, $pages ) ) !== false ){
+				unset( $pages[$key] );
+				$pages = array_values( $pages );
+			}
 
-		    $random_keys = array_rand( $pages, 4 );
+			$random_keys = array_rand( $pages, 4 );
 
-		    $pages = array(
-		        $pages[$random_keys[0]],
-		        $pages[$random_keys[1]],
-		        $pages[$random_keys[2]],
-		        $pages[$random_keys[3]]
-            );
+			$pages = array(
+				$pages[$random_keys[0]],
+				$pages[$random_keys[1]],
+				$pages[$random_keys[2]],
+				$pages[$random_keys[3]]
+			);
 
-	        return get_posts( array(
-		        'post_type'     => 'page',
-	            'numberposts'   => 4,
-	            'orderby'       => 'rand',
-	            'order'         => 'ASC',
-                'include'       => implode( ',', $pages ),
-            ) );
-        }
-    }
+			return get_posts( array(
+				'post_type'     => 'page',
+				'numberposts'   => 4,
+				'orderby'       => 'rand',
+				'order'         => 'ASC',
+				'include'       => implode( ',', $pages ),
+			) );
+		}
+	}
 }
