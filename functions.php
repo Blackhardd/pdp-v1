59aaa501487d94a8a88bc8d632050a1135e2d7da
Blackharddd <djalexmurcer@gmail.com>
2021-02-18 17:22:09 +0200

    Minor updates.

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if( !defined( '_S_VERSION' ) ){
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.15' );
+	define( '_S_VERSION', '1.0.16' );
 }
 
 if ( ! function_exists( 'pdp_setup' ) ) :
@@ -66,8 +66,6 @@ if ( ! function_exists( 'pdp_setup' ) ) :
 			'html5',
 			array(
 				'search-form',
-				'comment-form',
-				'comment-list',
 				'gallery',
 				'caption',
 				'style',
@@ -123,25 +121,6 @@ function pdp_content_width() {
 }
 add_action( 'after_setup_theme', 'pdp_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function pdp_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'pdp' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'pdp' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'pdp_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
