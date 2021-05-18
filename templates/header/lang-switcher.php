37b56317f3ab4470bdfefcfb6adc6910c37e67d8
Blackharddd <djalexmurcer@gmail.com>
2021-05-18 15:58:18 +0300

    Added function_exists() check.

--- templates/header/lang-switcher.php
+++ templates/header/lang-switcher.php
@@ -1,23 +1,23 @@
 <?php
+if( function_exists( 'pll_the_languages' ) ) :
+    $langs = pll_the_languages( array( 'raw' => true ) );
+    $current_lang = pll_current_language(); ?>
 
-$langs = pll_the_languages( array( 'raw' => true ) );
-$current_lang = pll_current_language();
+    <div class="lang-switcher">
+        <div class="lang-switcher__current">
+            <?=( $current_lang == 'uk' ) ? 'ua' : $current_lang; ?>
+            <svg width="14" height="9" fill="none">
+                <path d="M13 1L7 7 1 1" stroke="#111" stroke-width="2"/>
+            </svg>
+        </div>
 
-?>
-
-<div class="lang-switcher">
-    <div class="lang-switcher__current">
-	    <?=( $current_lang == 'uk' ) ? 'ua' : $current_lang; ?>
-        <svg width="14" height="9" fill="none">
-            <path d="M13 1L7 7 1 1" stroke="#111" stroke-width="2"/>
-        </svg>
+        <ul class="lang-switcher__list">
+            <?php foreach( $langs as $lang ) : ?>
+                <?php if( $lang['slug'] != $current_lang ) : ?>
+                    <li class="lang-switcher__item"><a href="<?=$lang['url']; ?>"><?=( $lang['slug'] == 'uk' ) ? 'ua' : $lang['slug']; ?></a></li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </ul>
     </div>
-
-    <ul class="lang-switcher__list">
-        <?php foreach( $langs as $lang ) : ?>
-            <?php if( $lang['slug'] != $current_lang ) : ?>
-                <li class="lang-switcher__item"><a href="<?=$lang['url']; ?>"><?=( $lang['slug'] == 'uk' ) ? 'ua' : $lang['slug']; ?></a></li>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </ul>
-</div>
+    <?php
+endif;
