eabb25c8d4639968ee6e26ecbf190afe5b445857
Blackharddd <djalexmurcer@gmail.com>
2021-05-18 13:53:27 +0300

    Theme version updated.

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if( !defined( '_S_VERSION' ) ){
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.23b' );
+	define( '_S_VERSION', '1.0.23c' );
 }
 
 if( !function_exists( 'pdp_setup' ) ) :
